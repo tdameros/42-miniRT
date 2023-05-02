68872ef766e45b7eac030dd4ad8b92f62e2abc37
Riescent <github.herd7@aleeas.com>
2023-05-02 14:47:13 +0200

    Renamed fonts .xpm

--- include/struct/t_window.h
+++ include/struct/t_window.h
@@ -1,10 +1,10 @@
 #ifndef T_WINDOW_H
 # define T_WINDOW_H
 
-//# define WINDOW_WIDTH 1280
-//# define WINDOW_HEIGHT 720
-# define WINDOW_WIDTH 1920
-# define WINDOW_HEIGHT 1080
+# define WINDOW_WIDTH 1280
+# define WINDOW_HEIGHT 720
+//# define WINDOW_WIDTH 1920
+//# define WINDOW_HEIGHT 1080
 
 typedef struct s_window
 {
