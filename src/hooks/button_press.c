14f10f34f9693ddbc99d02966ca46ccc52f61494
Riescent <github.herd7@aleeas.com>
2023-06-02 17:44:46 +0200

    Color picker color now defaults to selected object color

--- src/hooks/button_press.c
+++ src/hooks/button_press.c
@@ -18,6 +18,8 @@
 #include "ray_tracer_gui_api.h"
 #include "hooks.h"
 
+static void	update_color_picker_color(t_gui *gui);
+
 int	button_press_handler(int button, int x, int y, t_engine *engine)
 {
 	t_gui_box	*clicked_gui_box;
@@ -46,11 +48,22 @@ int	button_press_handler(int button, int x, int y, t_engine *engine)
 		}
 		ft_printf("selected object %s\n\n", tmp);
 		//!Testing
-
+		update_color_picker_color(&engine->gui);
 		return (update_object_attributes_modification_box(engine));
 	}
 	if (clicked_gui_box->on_click != NULL)
 		clicked_gui_box->on_click(clicked_gui_box, engine, y, x);
 	ft_printf("Clicked gui box\n\n");
 	return (0);
+}
+
+static void	update_color_picker_color(t_gui *gui)
+{
+	if (gui->selected_object == NULL)
+		return ;
+	gui->color_picker_base_color_was_changed = true;
+	gui->color_picker_base_color = (t_color){
+		.x = gui->selected_object->albedo.x * 255.f,
+		.y = gui->selected_object->albedo.y * 255.f,
+		.z = gui->selected_object->albedo.z * 255.f};
 }
