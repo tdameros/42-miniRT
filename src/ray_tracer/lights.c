d16fa4a242314d31c44298f4ebd78012c02774df
tdameros <tomdamerose>
2023-06-11 17:09:09 +0200

    enable shadows

--- src/ray_tracer/lights.c
+++ src/ray_tracer/lights.c
@@ -31,17 +31,17 @@ t_vector3f	calculate_color(const t_scene *scene, t_hit ray_hit)
 
 float	calculate_light_intensity(const t_scene *scene, t_hit ray_hit)
 {
-//	t_hit			light_hit;
-//	t_ray			light_ray;
+	t_hit			light_hit;
+	t_ray			light_ray;
 	t_vector3f		light_direction;
 	float 			light_intensity;
 
 	light_direction = vector3f_unit(vector3f_subtract(ray_hit.position, scene->light.position));
-//	light_ray = ray_create(vector3f_add(ray_hit.position, vector3f_multiply(ray_hit.normal, 0.001f)),
-//						   vector3f_multiply(light_direction, -1));
-//	light_hit = calculate_ray_intersection(light_ray, scene);
-//	if (light_hit.hit)
-//		return (0.1f);
+	light_ray = ray_create(vector3f_add(ray_hit.position, vector3f_multiply(ray_hit.normal, 0.001f)),
+						   vector3f_multiply(light_direction, -1));
+	light_hit = calculate_ray_intersection(&light_ray, scene);
+	if (light_hit.hit)
+		return (0);
 //		color = vector3f_multiply(color, 0.1f);
 	light_intensity = vector3f_dot(ray_hit.normal,
 								   vector3f_multiply(light_direction, -1));
